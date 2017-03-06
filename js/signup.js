d46a87103084a05084d5b3da3bed2479f51fb436
Sierra <sierramroberts.96@gmail.com>
2017-03-06 02:23:52 -0500

    .

--- js/signup.js
+++ js/signup.js
@@ -5,40 +5,40 @@ if (location.href.includes('logout')) {
 }
 
 function signup() {
-    // var name = document.querySelector('#name').value;
-    // var password = document.querySelector('#password').value;
-    // var phone = document.querySelector('#phone_number').value;
+    var name = document.querySelector('#name').value;
+    var password = document.querySelector('#password').value;
+    var phone = document.querySelector('#phone_number').value;
 
-    // // fetch('http://acc70ddc.ngrok.io/users', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
+    fetch('https://agile-shelf-15660.herokuapp.com/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
 
-    //     // Back-end controls the left side, properties, of this object
-    //     // Front-end controls the variables names and values on the right side
-    //     body: JSON.stringify({
-    //         username: name,
-    //         password: password,
-    //         phone_number: phone,
-    //     })
-    // })
-    //     .then(function(response) {
-    //         return response.json();
-    //     })
-    //     .then(function(response) {
-    //         // console.log(response);
+        // Back-end controls the left side, properties, of this object
+        // Front-end controls the variables names and values on the right side
+        body: JSON.stringify({
+            username: name,
+            password: password,
+            phone_number: phone,
+        })
+    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            // console.log(response);
 
-    //         if (response.token) {
-    //             // Saves any string into a named spot within your browser for the current domain.
-    //             sessionStorage.setItem('token', response.token);
-    //             location.href = 'chirp.html';
-    //         }
-    //         else {
-    //             alert('There was an error. Check out your console.');
-    //             console.log(response);
-    //         }
-    //     })
+            if (response.token) {
+                // Saves any string into a named spot within your browser for the current domain.
+                sessionStorage.setItem('token', response.token);
+                location.href = 'chirp.html';
+            }
+            else {
+                alert('There was an error. Check out your console.');
+                console.log(response);
+            }
+        })
         location.href = "chirp.html";
         console.log(location.href);
 }
